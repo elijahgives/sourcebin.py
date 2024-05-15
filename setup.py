5980cfb9e4e3261d6be1c6a3a1140384d072675c
Elijah <57592789+elijahgives@users.noreply.github.com>
2024-05-15 10:59:04 -0500

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name="sourcebin",
-    version="1.0.0",
+    version="1.0.1",
     author="author",
     author_email="hi@elijah.rip",
     description="Sourcebin API wrapper for Python",
